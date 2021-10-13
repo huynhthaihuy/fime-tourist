71f480fd5a9528f20515fc787e5cafa69441d75f
Nguyen Cong Nhat <51161694+ckan719@users.noreply.github.com>
2021-10-13 09:44:34 +0700

    feature(layout): add to pull alias

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -9,6 +9,16 @@ import {
 } from "@ant-design/icons";
 import "./index.css";
 import SubMenu from "antd/lib/menu/SubMenu";
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import {Cart} from "@components";
+
+
 const { Header, Sider, Content } = Layout;
 
 function Dashboard() {
@@ -23,38 +33,55 @@ function Dashboard() {
     };
 
     return (
-        <Layout>
-            <Sider trigger={null} collapsible collapsed={state.collapsed}>
-                <div className="logo">
-                    
-                </div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                    <SubMenu key="sub1" icon={<ShoppingOutlined />} title="Ecommerce">
-                        <Menu.Item key="1">Shop</Menu.Item>
-                        <Menu.Item key="2">Cart</Menu.Item>
-                        <Menu.Item key="3">Checkout</Menu.Item>
-                        <Menu.Item key="4">Cards</Menu.Item>
-                    </SubMenu>
-                </Menu>
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                    <div className="trigger" onClick={toggle}>
-                        {state.collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        <Router>
+            <Layout>
+                <Sider trigger={null} collapsible collapsed={state.collapsed}>
+
+                    <div className="logo">
+
                     </div>
-                </Header>
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        margin: "24px 16px",
-                        padding: 24,
-                        minHeight: 800,
-                    }}
-                >
-                    Content
-                </Content>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+                        <SubMenu key="sub1" icon={<ShoppingOutlined />} title="Ecommerce">
+                            <Menu.Item key="1"><Link to="/shop">Shop</Link></Menu.Item>
+                            <Menu.Item key="2"><Link to="/cart">Cart</Link></Menu.Item>
+                            <Menu.Item key="3"><Link to="/checkout">Checkout</Link></Menu.Item>
+                            <Menu.Item key="4"><Link to="/cards">Cards</Link></Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ padding: 0 }}>
+                        <div className="trigger" onClick={toggle}>
+                            {state.collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        </div>
+                    </Header>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: "24px 16px",
+                            padding: 24,
+                            minHeight: 800,
+                        }}
+                    >
+                        <Switch>
+                            <Route path="/cart">
+                                <Cart />
+                            </Route>
+                            <Route path="/cards">
+                                
+                            </Route>
+                            <Route path="/checkout">
+                                
+                            </Route>
+                            <Route path="/shop">
+                                
+                            </Route>
+                        </Switch>
+                    </Content>
+                </Layout>
             </Layout>
-        </Layout>
+        </Router>
+
     );
 }
 export default Dashboard;
